f6e32b15c930ffaef05c1aa8abcdcb65942d1d0b
Paul Pham <paul@invisible.college>
2016-09-15 17:55:55 -0400

    Add file to compile and a contract output directory.

---
+++ compile.js
@@ -0,0 +1,30 @@
+var solc = require('solc');
+var fs = require('fs');
+var Web3 = require('Web3');
+var web3 = new Web3();
+
+//var endpoint = "http://localhost:12345";
+var endpoint = "http://52.4.63.14:8545";
+
+web3.setProvider(new web3.providers.HttpProvider(endpoint));
+
+fs.readFile("hello.solc", function(err, data) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+  var source = data.toString(); // because it's a buffer
+  var compiled = web3.eth.compile.solidity(source);
+  var code = compiled.test.code;
+  var abi = compiled.test.info.abiDefinition;
+  
+  web3.eth.contract(abi).new({data: code}, function(err, contract) {
+    if (err) {
+      console.error(err);
+      return;
+    } else if (contract.address) {
+      console.log('address: ' + contract.address);
+    }
+  });
+});
