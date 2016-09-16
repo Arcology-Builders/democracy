b7ec7c4a21f34afe90908466ae1f3b39f3c0199e
Paul Pham <paul@invisible.college>
2016-09-16 03:16:13 +0000

    Add uploader script for SearchAgainstHumanity. Change case of web3 imports for unix.

--- compileSimpleStorage.js
+++ compileSimpleStorage.js
@@ -1,6 +1,6 @@
 //var solc = require('solc');
 var fs = require('fs');
-var Web3 = require('Web3');
+var Web3 = require('web3');
 var web3 = new Web3();
 
 //var endpoint = "http://localhost:12345";
