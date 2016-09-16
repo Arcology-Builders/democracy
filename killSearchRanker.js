04e58dac2dbe53a3517f3a16bdc752f15dbca39f
Paul Pham <paul@invisible.college>
2016-09-16 17:33:36 +0000

    Add working SimpleQuery and generated files.

--- killSearchRanker.js
+++ killSearchRanker.js
@@ -10,7 +10,7 @@ web3.setProvider(new web3.providers.HttpProvider(endpoint));
 var code = fs.readFileSync("contracts/SearchAgainstHumanity.bin").toString();
 var abi = JSON.parse(fs.readFileSync("contracts/SearchAgainstHumanity.abi").toString());
 
-var instance = web3.eth.contract(abi).at("0x02ed1159a582b517dcb0b63877cd6167deb85aca");
+var instance = web3.eth.contract(abi).at("0x7985de0dc5da5697e3ea57eb42d9ba8bf14d6ec1");
 
 var result = instance.kill({from: web3.eth.coinbase, gas: 70000});
 console.log("Result: " + result);
