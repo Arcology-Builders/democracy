27ed64fb4adeddf47f6066ba37f27fffedecb9fa
Paul Pham <paul@invisible.college>
2016-10-02 01:49:09 -0400

    Use config and integer fundingGoal and bigger gas in init script.

--- initZcashEscrow.js
+++ initZcashEscrow.js
@@ -1,12 +1,10 @@
-var fs = require('fs');
-var Web3 = require('web3');
-var web3 = new Web3();
+fs = require('fs')
+Web3 = require('web3')
+web3 = new Web3()
+config = require('config')
 
-//var endpoint = "http://localhost:12345";
-var endpoint = "http://52.4.63.14:8545";
-
-// v3 Address
-var address = "0xfe4a8853a3aa7c00e2cbfc1b95c1100213be20c9";
+endpoint = config.get("http_provider")
+address = config.get("contract_address")
 
 web3.setProvider(new web3.providers.HttpProvider(endpoint));
 
@@ -15,8 +13,8 @@ var abi = JSON.parse(fs.readFileSync("contracts/ZcashEscrow.abi").toString());
 
 var instance = web3.eth.contract(abi).at(address);
 
-var result = instance.initialize(0.01, [web3.eth.coinbase], new Date(Date.now() + 300).getTime(),
-    {from: web3.eth.coinbase, gas: 50000},
+var result = instance.initialize(1, [web3.eth.coinbase], new Date(Date.now() + 300).getTime(), web3.eth.coinbase,
+    {from: web3.eth.coinbase, gas: 250000},
     function(err, data, something) {
         console.log("err: " + err);
         console.log("data: " + data);
