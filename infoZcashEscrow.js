fb34c6ad28de1ff1abc6aa1e5fe9fe54f3494ae1
Paul Pham <paul@invisible.college>
2016-10-02 01:50:49 -0400

    Stop sending money to check deadline.

--- infoZcashEscrow.js
+++ infoZcashEscrow.js
@@ -1,22 +1,13 @@
 var fs = require('fs');
 var Web3 = require('web3');
 var web3 = new Web3();
+config = require('config')
 
 var contractName = "ZcashEscrow"
 console.log("Contract name: " + contractName);
 
-//var endpoint = "http://localhost:12345";
-//var endpoint = "http://52.4.63.14:8545";
-var endpoint = "http://testnet.local-box.org:8545";
-
-//v1 Address
-//var address = "0x8cf1a92eb3c945995c89602929e63bcdcba0a096"
-// v2 Address
-// var address = "0x7c6fccbf57b9fbcc10be81bfca05503392dca250"
-// v3 Address
-//var address = "0xfe4a8853a3aa7c00e2cbfc1b95c1100213be20c9"
-// v4 Address
-var address = "0x5e522be9c91ee6411426ec05650d97efbbcd8139";
+endpoint = config.get('http_provider')
+address = config.get('contract_address')
 
 web3.setProvider(new web3.providers.HttpProvider(endpoint));
 
@@ -37,6 +28,7 @@ console.log("Open: " + instance.open());
 console.log("Closed: " + instance.closed());
 console.log("Approved: " + instance.approved());
 
-var remoteTime = instance.checkDeadline.sendTransaction({from: web3.eth.coinbase, value: 1000000}, function(err, data) { console.log("Finished! " + err + data) });
+var remoteTime = instance.checkDeadline({from: web3.eth.coinbase, gas: 100000})
+        //function(err, data) { console.log("Finished! " + err + data) });
 
 console.log("Remote Time: " + remoteTime);
