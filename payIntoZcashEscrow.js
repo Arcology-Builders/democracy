c9e5ae390bc2c6b074c207af157e01f21227291f
Paul Pham <paul@invisible.college>
2016-10-02 01:51:13 -0400

    Use config values for pay script.

--- payIntoZcashEscrow.js
+++ payIntoZcashEscrow.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 var Web3 = require('web3');
-var web3 = new Web3();
+web3 = new Web3()
+
+config = require('config')
+
+address = config.get('contract_address')
+endpoint = config.get('http_provider')
 
 var contractName = "ZcashEscrow"
 console.log("Contract name: " + contractName);
 
-//var endpoint = "http://localhost:12345";
-//var endpoint = "http://52.4.63.14:8545";
-var endpoint = "http://testnet.local-box.org:8545";
-
-var address = "0x7c6fccbf57b9fbcc10be81bfca05503392dca250"
 web3.setProvider(new web3.providers.HttpProvider(endpoint));
 
 var code = fs.readFileSync(`contracts/${contractName}.bin`).toString();
@@ -19,4 +19,4 @@ var instance = web3.eth.contract(abi).at(address);
 
 console.log("Owner: " + instance.owner());
 
-web3.eth.sendTransaction({from: web3.eth.coinbase, to: address, value: web3.toWei(0.05, "ether")});
+web3.eth.sendTransaction({from: web3.eth.coinbase, to: address, value: web3.toWei(0.001, "ether")});
