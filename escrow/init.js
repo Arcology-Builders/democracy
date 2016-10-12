cb44f0c8110d8125b339c131476aa41ffcbb729b
Paul Pham <paul@invisible.college>
2016-10-11 23:17:36 -0400

    Fix payIntoVictim method. Add a fromAddr for custom escrow init attempts.

--- escrow/init.js
+++ escrow/init.js
@@ -1,9 +1,11 @@
 contract = new require('../contract')('ZcashEscrow')
 
+fromAddr = process.argv[2]
+
 func = (instance) => {
   instance.initialize(contract.web3.toWei(1, "ether"), [contract.web3.eth.coinbase],
           new Date(Date.now() + 1200).getTime() / 1000,
-    {from: contract.web3.eth.coinbase, gas: 250000},
+    {from: fromAddr, gas: 250000},
     function(err, data, something) {
         console.log("err: " + err);
         console.log("data: " + data);
