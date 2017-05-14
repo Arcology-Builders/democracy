c8eb94a99f5e6659a370893f668559d3efe7fa16
Paul Pham <paul@invisible.college>
2017-05-13 22:19:19 -0400

    Add Kovan endpoint and preamble for repl.

---
+++ preamble.js
@@ -0,0 +1,32 @@
+// Script to connect to JSONRPC and enter a REPL
+var Web3 = require('web3');
+var web3 = new Web3();
+
+config = require('config')
+
+Preamble = () => {
+    coinbase = config['coinbase']
+    console.log("Coinbase: " + coinbase)
+
+    if (process.argv.length < 2) {
+        console.error("Usage: node -i console.js [network]");
+    }
+
+    network = process.argv[2]
+    console.log("Net: " + network)
+
+    if (network === 'mainnet') {
+      console.log("Mainnet")
+      endpoint = config['endpoints']['mainnet']
+    } else {
+      console.log("kovan")
+      endpoint = config['endpoints']['kovan']
+    }
+
+    command = process.argv[3]
+
+    web3.setProvider(new web3.providers.HttpProvider(endpoint))
+    return { "web3": web3 }
+}
+
+module.exports = Preamble
