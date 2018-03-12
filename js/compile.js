f8e4bb408ccc52507c888f5b7545f3a816cec187
Paul Pham <paul@invisible.college>
2018-03-12 18:05:09 -0400

    Compilation now uses solcjs and outputs a single JSON with ABI and bytecode. Add OpenZeppelin contracts. yeeeaw

---
+++ js/compile.js
@@ -0,0 +1,66 @@
+// Compile with solcjs
+fs = require('fs')
+path = require('path')
+solc = require('solc')
+assert = require('assert')
+
+// Open contracts installed by npm -E zeppelin-solidity
+ZEPPELIN_PATH = "node_modules/zeppelin-solidity/contracts"
+// Open contracts from democracy
+DEMO_PATH = "src"
+inputs = {};
+
+queue = [ZEPPELIN_PATH, DEMO_PATH]
+
+while (queue.length > 0) {
+  f = queue.pop();
+  shortList = path.basename(f).split('.')
+  if (shortList.length > 1 && !shortList[1].startsWith('sol')) { continue; } // Skip hidden files like DS_Store
+  if (fs.lstatSync(f).isDirectory()) {
+    fs.readdirSync(f).forEach((f2) => queue.push(path.join(f,f2)))
+    //console.log(JSON.stringify(queue))
+  } else {
+    source = fs.readFileSync(f).toString();
+    //key = f.replace("node_modules/zeppelin-solidity/", "")
+    paths = f.split(path.sep)
+    keys = []
+    // This is a hack to push all suffixes of a contract path
+    // since it's not clear which one solcjs will request
+    // in findImport below
+    do {
+      keys.push(paths.join(path.sep))
+      //console.log(`${paths}`)
+      if (paths.length <= 1) { break; }
+      paths = paths.slice(-(paths.length - 1))
+    } while(true);
+    //console.log("Keys " + JSON.stringify(keys))
+    keys.forEach((key) => inputs[key] = source)
+  }
+}
+
+function findImports (path) {
+  //console.log(`Finding path ${path}`)
+  //console.log(`Returning path ${inputs[path]}`)
+  assert(inputs[path])
+  return {contents: inputs[path]}
+}
+
+sourceMap = {}
+for (var contract in inputs) {
+  console.log(`${contract}: ${inputs[contract].length}`)
+  shortName = path.basename(contract)
+  sourceMap[shortName] = inputs[contract]
+}
+outputs = solc.compile({sources: sourceMap}, 1, findImports)
+
+// Uncomment below to dump the output of solc compiler
+console.log(JSON.stringify(outputs.errors))
+for (var contractName in outputs.contracts) {
+  shortName = path.basename(contractName).split(":")[1]
+  output = {
+    name: shortName,
+    code: outputs.contracts[contractName].bytecode,
+    abi: JSON.parse(outputs.contracts[contractName].interface)
+  }
+  fs.writeFileSync(`outputs/${shortName}.json`, JSON.stringify(output))
+}
