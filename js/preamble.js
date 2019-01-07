a66439430b161d414da157d399cfc3a7819bede4
Paul Pham <paul@invisible.college>
2019-01-07 17:34:04 -0500

    Add back in web3 deps and simplify preamble.

--- js/preamble.js
+++ js/preamble.js
@@ -4,19 +4,12 @@ var web3 = new Web3();
 
 config = require('config')
 
-Preamble = () => {
+Preamble = (network, command) => {
     coinbase = config['coinbase']
     console.log("Coinbase: " + coinbase)
 
-    if (process.argv.length < 2) {
-        console.error("Usage: node -i console.js [network]");
-    }
-
-    network = process.argv[2]
     console.log("Net: " + network)
     endpoint = config['endpoints'][network]
-
-    command = process.argv[3]
 
     web3.setProvider(new web3.providers.HttpProvider(endpoint))
     return { "web3": web3 }
