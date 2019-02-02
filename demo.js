359959a1b9b9b566e253bfee07512782c00069d7
Paul Pham <paul@invisible.college>
2019-02-02 03:18:59 +0000

    Major refactoring of demo.js with more uniform args. Add eslint config, still not ideal.

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 // demo.js, the entry point for Democracy, an undiscovered decentralized country
 
-const { Seq } = require('immutable')
+const { Map, Seq } = require('immutable')
 const path = require('path')
 
 // Arg numbers might depend on how we invoke this script
@@ -11,46 +11,41 @@ const start = (path.basename(process.argv[0]) === 'node') ? 1 : 0
 const command      = process.argv[start]   
 const subcommand   = process.argv[start+1]
 
-/*
-if (process.argv.length < start+1) {
-  console.log(`Usage ${command} [subcommand]`)
-  process.exit(1)
-}
-*/
 console.log(`Command ${command}`)
 console.log(`Subcommand ${subcommand}`)
 
 // Menu of opt/arg processors to use in each subcommand below
 
-getNetwork = (nextArgIndex) => {
-  network = process.argv[nextArgIndex]
-  console.log(`Network ${network}`)
-  return require('./js/preamble')(network)
+argsOrDie = (args, msg) => {
+  if (args.length == 0) {
+    console.log(`\n  ${path.basename(command)} ${subcommand} ${msg}\n`)
+    process.exit(1)
+  }
 }
 
-eth = {}
-accounts = []
+getNetwork = (netName) => {
+  console.log(`Network ${netName}`)
+  return require('./js/preamble')(netName)
+}
 
-getAccounts = async (nextArgIndex) => {
-  eth = getNetwork(nextArgIndex)
-  accounts = await eth.accounts()
+getAccounts = async (network) => {
+  accounts = await network.accounts()
   return accounts
 }
 
-doBalances = async (nextArgIndex) => {
-  eth = getNetwork(nextArgIndex)
-  showBalances = true // change this later if you want it to be optional
-  const accounts = await getAccounts()
-  accounts.map(async (address, i) => {
-    let balance = await eth.getBalance(address).then((value) => { return value.toString() }) 
-    let balanceString = showBalances ? `\t${balance} wei` : ''
-    console.log(`accounts[${i}] ${address} ${balanceString}`)
-  })    
+getAccountFromArg = (accounts, arg) => {
+  console.log(`${arg}`)
+  if (arg.startsWith('account')) {
+    let accountIndex = parseInt(arg.slice(7))
+    return accounts[accountIndex]
+  } else {
+    throw new Error("Arg ${arg} should be of the form 'accountx'")
+  }
 }
 
 const { traverseDirs } = require('./js/utils')
 
-getContractNames = () => {
+getContracts = () => {
   const contractSources = []
   const contractOutputs = {}
   traverseDirs(
@@ -74,74 +69,78 @@ getContractNames = () => {
     }
   )
   return {
-    contractSources: contractSources,
-    contractOutputs: contractObjects
+    contractSources: Seq(contractSources),
+    contractOutputs: Map(contractOutputs)
   }
 }
-
-const ethUtil = require('ethereumjs-util')
 
 // Return a map of { 'dependencyName' : 'deployId' }
-getDepMap = (indexFromStart) => {
-  depMap = {}
-  for (var i = process.argv[start+index]; i < process.argv.length; i++) {
-    const [ dep, deployId ] = process.argv[i].split(':')
+getDepMap = (argsUntilEnd) => {
+  return argsUntilEnd.map((arg) => {
+    const [ dep, deployId ] = arg.split(':')
     if (!dep || !deployId) {
-      throw new Error(`Invalid dependency:address pair ${process.argvi[i]}`)
+      throw new Error(`Invalid dependency:address pair ${arg}`)
     }
-    depMap[dep] = deployId
-  }
-  return depMap
+  })
 }
 
-getCompile = async() => {
-  compileName = process.argv[start+2]
-  console.log(`Compile Name ${compileName}`)
-  require('./js/compile')(compileName)
+doBalances = async (eth) => {
+  const showBalances = true // change this later if you want it to be optional
+  const accounts = await getAccounts(eth)
+  accounts.map(async (address, i) => {
+    let balance = await eth.getBalance(address).then((value) => { return value.toString() }) 
+    let balanceString = showBalances ? `\t${balance} wei` : ''
+    console.log(`accounts[${i}] ${address} ${balanceString}`)
+  })    
 }
 
-getArg = (indexFromStart) => {
-  let index = start + indexFromStart,
-      arg = process.argv[start+indexFromStart]
-  if (!arg) throw new Error(`Missing arg at index ${index}`)
-  return arg 
-}
-
-// Find out what available contract names we have
-getContractNames()
 
 async function main() {
 
   TABLE = {
-    'accounts': () => { doBalances() },
-    'compile' : () => { require('./js/compile')  },
-  }
 
-  CONTRACT_SUBTABLE = {
-    ''        : (contractName) => { console.log(JSON.stringify(contractOutputs[contractName])) },
-    'link'    : (contractName) => { require('./js/link')(getNetwork(3), contractName, getDepMap(4)) }, 
-    'deploy'  : async(contractName) => {
-	accounts = await getAccounts()
-	// TODO This needs to be cleaned up, especially to unlock accounts for public spending
-	if (process.argv[start+4].startsWith('account')) {
-	  console.log(process.argv[start+4])
-	  let accountIndex = parseInt(process.argv[start+4].slice(7))
-	  let deployerAddr = accounts[accountIndex]
-	  console.log(`Spending from account ${accountIndex}: ${deployerAddr}`)
-          require('./js/deploy')(getNetwork(3), deployerAddr, contractOutputs[contractName], process.argv[start+5])
-	} }, 
+    undefined        : () => {
+      const { contractOutputs } = getContracts() 
+      contractOutputs.map((out) => { console.log(JSON.stringify(out, null, '  ')) })
+    },
+
+    'accounts': (args) => { doBalances(getNetwork(args[0])) },
+
+    'info'    : (args) => {
+      const { contractOutputs } = getContracts() 
+      console.log(JSON.stringify(contractOutputs[args[0]], null, '  '))
+    },
+
+    'compile' : (args) => {
+      // compile [0 ContractName]
+      require('./js/compile')(args[0])
+    },
+	      
+    'link'    : (args) => {
+      argsOrDie(args,
+        '<0 ContractName> <1 netName> [2 depLink1:depDeploy1 depLink2:depDeploy2 ... ]')
+      const { contractOutputs } = getContracts()
+      const contract     = contractOutputs[args[0]]
+      const net          = getNetwork(args[1])
+      const linkMap      = getDepMap(args.slice(2))
+      require('./js/link')(contract, net, linkMap)
+    },
+
+    'deploy'  : async (args) => {
+      argsOrDie(args,
+	'<0 ContractName> <1 netName> <2 deployerAddr> <3 deployId>')
+      const { contractOutputs } = getContracts()
+      const contract     = contractOutputs[args[0]]
+      const net          = getNetwork(args[1])
+      const accounts     = await getAccounts(net)
+      const deployerAddr = getAccountFromArg(accounts, args[2])
+      const deployId     = args[3]
+      require('./js/deploy')(net, deployerAddr, contract, deployId)
+    }, 
   }
 
   subcommand && console.log(`${subcommand}`)
-  if (!subcommand) {
-    console.log("Available Contract Names:");
-  } else if (TABLE[subcommand]) {
-    TABLE[subcommand](contractOutputs[process.argv[start+2])
-  } else if (contractOutputs[subcommand]) {
-    subsubcommand = process.argv[start+2] || ''
-    console.log(`subsubcommand ${subsubcommand}`)
-    CONTRACT_SUBTABLE[subsubcommand](subcommand)
-  } 
+  TABLE[subcommand](process.argv.slice(start+2))
 
 }
 
