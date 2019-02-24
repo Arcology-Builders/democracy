578a8b89745368b85ca237641e894f82a5009bba
Paul Pham <paul@invisible.college>
2019-02-23 23:18:46 -0500

    Add separate test for accounts. Bump to 0.1.10 for working CircleCI integration and explicit package versions.

---
+++ tests/accounts.spec.js
@@ -0,0 +1,30 @@
+require('module-alias/register')
+const demo = require('@root')
+const { print, getInstance } = require('@democracy.js/utils')
+
+const BN = require('bn.js')
+const chai = require('chai').use(require('chai-as-promised'));
+const assert = chai.assert
+const should = chai.should(); 
+const Eth = require('ethjs')
+
+const config = require('config')
+
+describe('Getting accounts and balances.', () => {
+
+  let eth
+  let networkId
+  let accounts
+
+  before(async () => {
+      eth = demo.getNetwork('test')
+      networkId = await eth.net_version()
+      accounts = await demo.getAccounts(eth)
+  })
+
+  it("return ten accounts.", async () => {
+    assert.equal(accounts.length, 10)
+    assert.typeOf(accounts[0], "string")
+  })
+
+})
