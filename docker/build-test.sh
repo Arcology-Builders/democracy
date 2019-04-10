663bbd56d2356d24b83c0e273dbeca143ca19bc4
Paul Pham <paul@invisible.college>
2019-04-10 01:20:39 -0400

    Add g++ to test image, tag with gitref.

--- docker/build-test.sh
+++ docker/build-test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+GITREF=$(git log | head -n 1 | cut -d ' ' -f 2 | head -c 7)
 
 docker build -f Dockerfile.test -t cryptogoth/node-circle-test .
+docker tag cryptogoth/node-circle-test:latest cryptogoth/node-circle-test:${GITREF} 
