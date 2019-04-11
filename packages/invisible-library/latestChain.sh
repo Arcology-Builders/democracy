c77cadd8e5703765e77a59ee417c86426b626e11
Paul Pham <paul@invisible.college>
2019-04-10 23:37:03 -0400

    Invisible Library should be moved out into its own repo.

--- packages/invisible-library/latestChain.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-LATEST=$(ls deploys | tail -n 1)
-if [ ! -e deploys/latest ] ; then
-  cd deploys; ln -s $LATEST ./latest
-fi
--- packages/invisible-library/webpack.js
+++
@@ -1,8 +0,0 @@
-const webpack = require('webpack')
-
-const demo = require('@democracy.js/utils')
-const eth = demo.getNetwork()
-
-eth.net_version().then((chainId) => {
-  return webpackConfig = require('./webpack.config')(chainId)
-}).then(webpack)
