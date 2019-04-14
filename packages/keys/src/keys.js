a4ed707c064f975235a32082270e9817b6a0a839
Paul Pham <paul@invisible.college>
2019-04-14 04:28:25 -0400

    Publish keys@0.0.7 with support for encrypting with passwords.

--- packages/keys/src/keys.js
+++ packages/keys/src/keys.js
@@ -3,29 +3,37 @@ const utils = require('ethereumjs-utils')
 const { Map } = require('immutable')
 const assert = require('chai').assert
 const bops = require('bops')
+const randombytes = require('randombytes')
+
+const PARAMS = { keyBytes: 32, ivBytes: 16 };
 
 const create = () => {
-  const account = keythereum.create()
+  const account = keythereum.create(PARAMS)
   const privateBuffer = account.privateKey
   const publicBuffer = utils.privateToPublic(account.privateKey)
   const addressBuffer = utils.privateToAddress(account.privateKey)
 
-  return bufferToMap(privateBuffer, publicBuffer, addressBuffer)
+  return bufferToMap(privateBuffer, publicBuffer, addressBuffer, account.iv, account.salt)
 }
 
-const bufferToMap = (_privateBuffer, _publicBuffer, _addressBuffer) => {
-  
+const bufferToMap =
+  (_privateBuffer, _publicBuffer, _addressBuffer, _ivBuffer, _saltBuffer) => {
+ 
+  assert.equal(_ivBuffer.length, PARAMS.ivBytes)
+  assert.equal(_saltBuffer.length, PARAMS.keyBytes)
+
   const privateString = hex(_privateBuffer)
   const publicString  = hex(_publicBuffer)
   const addressString = hex(_addressBuffer)
+  const ivString      = hex(_ivBuffer)
+  const saltString    = hex(_saltBuffer)
 
   return new Map({
-    privateBuffer   : _privateBuffer,
-    publicBuffer    : _publicBuffer,
-    addressBuffer   : _addressBuffer,
     privateString   : privateString,
     publicString    : publicString,
     addressString   : addressString,
+    ivString        : ivString,
+    saltString      : saltString,
     privatePrefixed : prefix(privateString),
     publicPrefixed  : prefix(publicString),
     addressPrefixed : prefix(addressString),
@@ -33,32 +41,65 @@ const bufferToMap = (_privateBuffer, _publicBuffer, _addressBuffer) => {
 }
 
 const createFromPrivateString = (_privateString) => {
-  const privateBuffer = Buffer(bops.from(_privateString, 'hex'))
+  const privateBuffer = Buffer.from(bops.from(_privateString, 'hex'))
   const publicBuffer = utils.privateToPublic(privateBuffer)
   const addressBuffer = utils.privateToAddress(privateBuffer)
-  return bufferToMap(privateBuffer, publicBuffer, addressBuffer)
+  const ivBuffer = randombytes(PARAMS.ivBytes)
+  const keyBuffer = randombytes(PARAMS.keyBytes)
+  return bufferToMap(privateBuffer, publicBuffer, addressBuffer, ivBuffer, keyBuffer)
+}
+
+const createFromPrivateBuffer = (_privateBuffer, _ivBuffer, _saltBuffer) => {
+  const publicBuffer = utils.privateToPublic(_privateBuffer)
+  const addressBuffer = utils.privateToAddress(_privateBuffer)
+  return bufferToMap(_privateBuffer, publicBuffer, addressBuffer, _ivBuffer, _saltBuffer)
 }
 
 const isAccount = (_map) => {
-  assert(utils.isValidPrivate(_map.get('privateBuffer')))
-  assert(utils.isValidPublic(_map.get('publicBuffer')))
-  assert(utils.isValidAddress(_map.get('addressPrefixed')))
 
   return (Map.isMap(_map)             &&
-          _map.get('privateBuffer'  ) &&
-          _map.get('publicBuffer'   ) && 
-          _map.get('addressBuffer'  ) &&
           _map.get('privateString'  ) &&
           _map.get('publicString'   ) &&
           _map.get('addressString'  ) &&
           _map.get('privatePrefixed') &&
           _map.get('publicPrefixed' ) &&
-          _map.get('addressPrefixed')
+          _map.get('addressPrefixed') &&
+          utils.isValidPrivate(bops.from(_map.get('privateString'), 'hex')) &&
+          utils.isValidPublic(bops.from(_map.get('publicString'), 'hex'))   &&
+          utils.isValidAddress(_map.get('addressPrefixed'))
          )
+}
+
+var OPTIONS = {
+  kdf: "pbkdf2",
+  cipher: "aes-128-ctr",
+  kdfparams: {
+    c: 262144,
+    dklen: 32,
+    prf: "hmac-sha256"
+  }
+}
+
+const accountToEncryptedJSON = (_account, _password) => {
+  return keythereum.dump(_password, buffer(_account.get('privateString')),
+    buffer(_account.get('saltString')), buffer(_account.get('ivString')), OPTIONS)
+}
+
+const encryptedJSONToAccount = (_keyObject, _password) => {
+  const privateBuffer = keythereum.recover(_password, _keyObject)
+  const ivString = _keyObject.crypto.cipherparams.iv
+  assert.equal(ivString.length, PARAMS.ivBytes * 2)
+  const saltString = _keyObject.crypto.kdfparams.salt 
+  assert.equal(saltString.length, PARAMS.keyBytes * 2)
+  return createFromPrivateBuffer(privateBuffer, buffer(ivString), buffer(saltString))
 }
 
 const hex = (_buffer) => {
   return _buffer.toString('hex')
+}
+
+const buffer = (_hex) => {
+  return bops.from(_hex, 'hex')
 }
 
 const prefix = (_str) => {
@@ -68,5 +109,7 @@ const prefix = (_str) => {
 module.exports = {
   isAccount              : isAccount,
   create                 : create,
-  createFromPrivateString: createFromPrivateString
+  createFromPrivateString: createFromPrivateString,
+  accountToEncryptedJSON : accountToEncryptedJSON,
+  encryptedJSONToAccount : encryptedJSONToAccount,
 }
--- packages/keys/tests/keys.spec.js
+++ packages/keys/tests/keys.spec.js
@@ -1,7 +1,16 @@
-const assert = require('chai').assert
+const chai = require('chai')
+const assert = chai.assert
+const expect = chai.expect
+chai.use(require('chai-as-promised'))
+
 const utils = require('ethereumjs-utils')
 const keys = require('../src/keys')
 const randombytes = require('randombytes')
+const { getImmutableKey, setImmutableKey, fromJS, Logger, deepEqual }
+  = require('@democracy.js/utils')
+const LOGGER = new Logger('keys.spec')
+
+const { Map } = require('immutable')
 
 describe('Self-generated keys and signing', () => {
  
@@ -17,14 +26,32 @@ describe('Self-generated keys and signing', () => {
 
   it('generates a valid key randomly from scratch', async () => {
     assert(utils.isValidAddress(account.get('addressPrefixed')))
-    assert.equal(40 , account.get('addressString'     ).length)
-    assert.equal(42 , account.get('addressPrefixed'   ).length)
+    assert.equal(40 , account.get('addressString'  ).length)
+    assert.equal(42 , account.get('addressPrefixed').length)
     assert.equal(64 , account.get('privateString'  ).length)
-    assert.equal(66, account.get('privatePrefixed').length)
+    assert.equal(66 , account.get('privatePrefixed').length)
     assert.equal(128, account.get('publicString'   ).length)
     assert.equal(130, account.get('publicPrefixed' ).length)
+    assert.equal(32 , account.get('ivString'       ).length)
+    assert.equal(64 , account.get('saltString'     ).length)
     const address = utils.publicToAddress(account.get('publicPrefixed')).toString('hex')
     assert.equal(address, account.get('addressString'))
+  })
+
+  it( 'dumps a password and recovers it', async () => {
+    const password = randombytes(32).toString('hex')
+    const password2 = randombytes(32).toString('hex')
+    const encryptedJSON = keys.accountToEncryptedJSON(account, password)
+    setImmutableKey('keys', fromJS(encryptedJSON))
+    const retrieved = getImmutableKey('keys', new Map({}))
+    const recovered = keys.encryptedJSONToAccount(retrieved.toJS(), password)
+    assert.equal(recovered.get('privateString'), account.get('privateString'))
+    let recovered2
+    try {
+      recovered2 = keys.encryptedJSONToAccount(retrieved.toJS(), password2)
+      assert.fail()
+    } catch(e) {
+    }
   })
 
   it('generates a valid wallet from private key', async () => {
@@ -33,6 +60,10 @@ describe('Self-generated keys and signing', () => {
     keys.isAccount(account)
     const account2 = keys.createFromPrivateString(randombytes(32).toString('hex'))
     keys.isAccount(account2)
+  })
+
+  after(() => {
+    setImmutableKey('keys', null)
   })
 
 })
