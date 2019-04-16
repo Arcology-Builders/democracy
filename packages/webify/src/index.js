5eb49275d79c850609fdf67d7333679ab8198348
Paul Pham <paul@invisible.college>
2019-04-16 02:05:48 -0400

    Update webify to include contract@0.0.5

--- packages/webify/src/index.js
+++ packages/webify/src/index.js
@@ -13,19 +13,12 @@ BrowserFS.FileSystem.LocalStorage.Create(function(e, lsfs) {
   });
 });
 
-//const { getConfig, getImmutableKey, setImmutableKey, Logger } = require('@democracy.js/utils')
-
 require('dotenv').config()
 const utils = require('@democracy.js/utils')
+const contract = require('@democracy.js/contract')
 const util = require('ethereumjs-utils')
 const keys = require('@democracy.js/keys')
-/*
-const LOGGER = new Logger('src/index')
 
-submodule = require('./submodule')
-LOGGER.info(`Submodule from Import  ${JSON.stringify(submodule)}`)
-LOGGER.info(`Hello Config ${JSON.stringify(config)}`)
-*/
 const fs = require('fs')
 const path = require('path')
 const { List } = require('immutable')
@@ -35,10 +28,6 @@ fs.writeFileSync(path.join('a', 'b.json'), "catcow")
 const buffer = fs.readFileSync("a/b.json")
 console.log(buffer.toString())
 assert(buffer.toString() === 'catcow')
-
-//const demo = require('democracy.js')
-//setImmutableKey("a/d", new List([1,2,3]))
-//console.log(getImmutableKey("a/d").toString())
 
 window.fs = fs
 window.path = path
