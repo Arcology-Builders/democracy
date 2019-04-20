b40ba1bc083cb8f53da7035eed8de95e3653b4d2
cryptogoth <paul@invisible.college>
2019-04-20 21:21:55 +0000

    [democracy.js][utils] update zeppelin path. disable demo.js tests for now.

--- packages/utils/src/utils.js
+++ packages/utils/src/utils.js
@@ -17,7 +17,7 @@ utils.DEPLOYS_DIR  = 'deploys'
 utils.LIB_PATTERN  = /__(([a-zA-Z0-9])+\/*)+\.sol:[a-zA-Z0-9]+_+/g
 
 utils.DEMO_SRC_PATH = 'contracts'
-utils.ZEPPELIN_SRC_PATH = 'node_modules/openzeppelin-solidity/contracts'
+utils.ZEPPELIN_SRC_PATH = '../../node_modules/openzeppelin-solidity/contracts'
 
 utils.setUtilsFS = (_fs) => {
   utils.fs = _fs
