7545d2ba7ddbdb734e1590f2f07b8909699ec789
Paul Pham <paul@invisible.college>
2019-04-22 23:48:15 -0400

    Remove webify as it's no longer needed.

--- wip/webify/src/index.js
+++
@@ -1,9 +0,0 @@
-config = require('config')
-//path = require('path')
-//import submod2 from "./submodule"
-
-//submodule = require(path.join(__dirname, 'submodule'))
-console.log(`Submodule from Import  ${JSON.stringify(submodule)}`)
-//console.log(`Submodule ${JSON.stringify(submodule)}`)
-//console.log(`Config ${JSON.stringify(config)}`)
-console.log(`Hello Config ${config['testKey']}`)
