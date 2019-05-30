bf89766e067f90703fc452f24c86bfb6f1b91e2e
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2019-05-30 18:12:56 -0400

    Add test contracts for relinking. Move script for starting ganache, make restartable.

---
+++ packages/depart/contracts/Relink.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.5.0;
+
+contract Relink {
+
+    uint256 public a;
+
+    constructor() public {
+        self.a = 0;
+    }
+
+    function outward(uint256 _a) {
+        self.a = _a
+    }
+
+}
