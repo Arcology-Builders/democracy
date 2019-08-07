0063f0981f340d804467d6cd6b7a784ec6c85994
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2019-08-07 17:08:38 -0400

    Fix getBalance tool using fromWei

--- packages/keys/bin/getBalance.js
+++ packages/keys/bin/getBalance.js
@@ -3,6 +3,7 @@
 
 const { getNetwork, getConfig } = require('demo-utils')
 const { fromWei } = require('ethjs-unit')
+const BN = require('bn.js')
 
 const eth = getNetwork()
 
@@ -12,7 +13,7 @@ const mainFunc = async () => {
   const chainId = await eth.net_version()
   console.log(`Balance of ${address} on chain ${chainId}`)
   const balance = await eth.getBalance(address)
-  console.log(`${fromWei(balance)} ETH`)
+  console.log(`${fromWei(balance, 'ether')} ETH`)
 }
 
 mainFunc()
