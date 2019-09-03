a9f686ce27a8451f6a71731f5d3abd47475c129a
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2019-09-03 16:58:22 -0400

    [depart] Add testing bin/scripts with new mainFunc

--- packages/depart/bin/simple.js
+++ packages/depart/bin/simple.js
@@ -5,15 +5,20 @@
 // A simple script to test a production-style departure
 // outside of a unit test using .env and TEST.DEPLOYER_ADDRESS
 
+const { Map } = require('immutable')
 const { Logger } = require('demo-utils')
 const LOGGER = new Logger('bin/simple')
-const { deployerMixin, departMixin, argListMixin, run } = require('..')
+const { deployerMixin, argListMixin, run } = require('demo-transform')
+const { departMixin } = require('..')
 
-const m0 = argListMixin(['anotherThing'])
+const m0 = argListMixin(Map({
+  anotherThing: '',
+  unlockSeconds: 3,
+  sourcePathList: ['../../node_modules/demo-test-contracts/contracts', 'contracts'],
+}))
 const m1 = deployerMixin({ unlockSeconds: 10 })
 const m2 = departMixin({
   name: 'Simple departure',
-  sourcePath: '../../node_modules/demo-test-contracts/contracts',
 })
 const departFunc = async (state) => {
   const { compile, link, deploy, bm, deployerEth, deployerAddress, anotherThing } = state.toJS()
@@ -25,4 +30,4 @@ const departFunc = async (state) => {
   LOGGER.info(`Deployed DifferentSender at ${ds.get('deployAddress')}`)
 }
 
-run( departFunc, [ m0, m1, m2 ] )
+run( [ m0, m1, m2, departFunc ] )
--- packages/depart/test.sh
+++ packages/depart/test.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 NODE_ENV=DEVELOPMENT node bin/simple.js AAA
+NODE_ENV=DEVELOPMENT ../../node_modules/.bin/demo-depart --departFileName departNew.js
