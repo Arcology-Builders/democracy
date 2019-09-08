83f18f8136f4fe9c492d868b45979213b4030a93
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2019-09-08 06:28:57 -0400

    [webify] Add clientInit here instead of leaving it to the calling code.

--- packages/webify/exports/api.js
+++ packages/webify/exports/api.js
@@ -102,6 +102,25 @@ api.init = async({ unlockSeconds }) => {
   }
 }
 
+api.clientInit = async() => {
+	await demo.initFS({})
+	await demo.init({ unlockSeconds: 100000 }) // keep unlocked as long as we cache password
+	await demo.prepareCachedWallet({})
+	await demo.prepareErasePassword({
+		erasePasswordSeconds: 100000, // 100,000 seconds is 27.77 hours
+		erasePasswordCallback: () => { console.log("Erasing password") },
+	})
+	await demo.prepareUpdateWhileCached({
+		updateSeconds: 10,
+		updateCallback: (secondsLeft) => { console.log(`${secondsLeft} seconds left`) },
+	})
+	demo.chainId = await demo.eth.net_version()
+	const thisPassword = localStorage.getItem(`demo/${demo.chainId}/thisPassword`)
+	const { signerEth } = await demo.keys.wallet.prepareSignerEth({
+		address: demo.thisAddress, password: thisPassword })
+	demo.thisSignerEth = signerEth
+}
+
 /**
  * Prepare, and if necessary, create and/or load, the Ethereum wallet (address/password)
  * combo for this browser.
