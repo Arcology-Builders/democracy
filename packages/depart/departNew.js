52c2f28a107d7ffeaefd8ec06b555c249aef9507
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2019-09-26 22:39:28 -0400

    [depart] restore from branch-0.3.7

--- packages/depart/departNew.js
+++ packages/depart/departNew.js
@@ -1,20 +1,19 @@
 'use strict'
-const BN      = require('bn.js')
-const assert  = require('chai').assert
-const { Map } = require('immutable')
+const BN = require('bn.js')
+const assert = require('chai').assert
 
-depart(Map({
+depart({
   departName        : 'Test Departure',
   sourcePathList    : ['contracts-new'],
   compileFlatten    : true,
   compileOutputFull : true,
-}), async ({ deployed, minedTx }) => {
+}, async ({ deployed, minedTx }) => {
   const relink = await deployed( 'Relink' )
  
   //const instance = await relink.getInstance() 
-  const txReceipt = await minedTx( relink.outward, [new BN(2224), true] )
+  const txReceipt = await minedTx( relink.outward, [new BN(2223)] )
   const a = (await relink.a())['0']
-  assert( a.eq(new BN(2224)), `Returned public member is not the one we set.` ) 
+  assert( a.eq(new BN(2223)), `Returned public member is not the one we set.` ) 
  
   return 22
 })
