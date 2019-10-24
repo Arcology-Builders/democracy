f9c07cad04e1c630a26070282d5a6184a837d58e
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2019-10-24 13:47:44 -0400

    [depart] Pass in depart options to departFunc.

--- packages/depart/departNew.js
+++ packages/depart/departNew.js
@@ -5,11 +5,14 @@ const BN = require('bn.js')
 const assert = require('chai').assert
 
 depart(Map({
+  someSymbol        : Map({ type: TYPES.string , value: 'a mirage'  }),
   departName        : Map({ type: TYPES.string , value: 'Test Departure'  }),
   sourcePathList    : Map({ type: TYPES.array  , value: ['contracts-new'] }),
   compileFlatten    : Map({ type: TYPES.boolean, value: true }),
   compileOutputFull : Map({ type: TYPES.boolean, value: true }),
-}), async ({ deployed, minedTx }) => {
+}), async ({ deployed, minedTx, someSymbol }) => {
+  
+  assert.equal( someSymbol, 'a mirage', `someSymbol did not make it through to departFunc` )
   const relink = await deployed( 'Relink' )
  
   //const instance = await relink.getInstance() 
