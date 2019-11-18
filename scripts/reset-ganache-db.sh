8640997e880a17a62a6f4e8321ee13608ac593ab
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2019-11-17 23:59:02 -0500

    Snapshot ganache-db and scripts.

---
+++ scripts/reset-ganache-db.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -Rf ~/src/private-trading/ganache-db
+mkdir ~/src/private-trading/ganache-db
---
+++ scripts/start-ganache.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ganache-cli -p 8545 -h 0.0.0.0 -i 2222 --debug --db ./ganache-db --mnemonic "tongue diet patch yard argue amount say speed nut seek creek spot" -d
