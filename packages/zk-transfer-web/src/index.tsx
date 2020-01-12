48d74f643d07dcd608d47e0bc418d117a77109c9
Joseph Julius <Joseph.owonwo@gmail.com>
2020-01-12 19:11:31 +0100

    [zk-transfer-web] fix: :wrench: error when NODE_ENV changes to RINKEBY
    Fixes  #41

--- packages/zk-transfer-web/src/index.tsx
+++ packages/zk-transfer-web/src/index.tsx
@@ -8,19 +8,9 @@ import * as serviceWorker from "./serviceWorker";
 document.addEventListener('DOMContentLoaded', async () => {
     const { demo } : any = window;
 
-    await demo.initFS({})
-    await demo.init({ unlockSeconds: 100000 }) // keep unlocked as long as we cache password
-    await demo.prepareCachedWallet({})
-    await demo.prepareErasePassword({
-        erasePasswordSeconds: 100000, // 100,000 seconds is 27.77 hours
-        erasePasswordCallback: () => { console.log("Erasing password") },
-    })
-    await demo.prepareUpdateWhileCached({
-        updateSeconds: 10,
-        updateCallback: (secondsLeft: number) => { console.log(`${secondsLeft} seconds left`) },
-    })
+    await demo.clientInit();
     console.log('Before rendering', demo);
-
+        
     ReactDOM.render(<App demo={demo}/>, document.getElementById("root"));
 });
 
