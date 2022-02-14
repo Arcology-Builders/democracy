939cee7c529e44533d3dbb1a21cc24c7a7a3f523
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2022-02-14 12:53:58 -0700

    Update docker test image to use node 16 and optional building for Apple M1 / arm64 , the previous node12 is too old for some packages.

--- docker/build-test.sh
+++ docker/build-test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 GITREF=$(git log | head -n 1 | cut -d ' ' -f 2 | head -c 7)
 
-docker build -f Dockerfile.test -t cryptogoth/node-circle-test .
-docker tag cryptogoth/node-circle-test:latest cryptogoth/node-circle-test:${GITREF} 
+NODE_VERSION=16.14
+
+docker build --platform linux/arm64/v8 -f docker/Dockerfile.test -t cryptogoth/node-circle-test:${NODE_VERSION} .
+docker tag cryptogoth/node-circle-test:${NODE_VERSION} cryptogoth/node-circle-test:${GITREF} 
